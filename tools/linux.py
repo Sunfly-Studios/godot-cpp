d21797f39415e6fc8aef807f83c97551681ff69e
MBCX <github.small792@passinbox.com>
2026-01-17 18:12:00 -0400

    Updated `SPARC64`

--- tools/linux.py
+++ tools/linux.py
@@ -43,7 +43,7 @@ def generate(env):
         env.Append(CCFLAGS=["-march=loongarch64", "-mabi=lp64d"])
         env.Append(LINKFLAGS=["-march=loongarch64", "-mabi=lp64d"])
     elif env["arch"] == "sparc64":
-        env.Append(CCFLAGS=["-mcpu=ultrasparc", "-m64"])
+        env.Append(CCFLAGS=["-mcpu=ultrasparc", "-m64", "-fno-strict-aliasing"])
         env.Append(LINKFLAGS=["-mcpu=ultrasparc", "-m64"])
     elif env["arch"] == "mips64":
         env.Append(
